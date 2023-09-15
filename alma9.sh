4a2a7697f8995035bce3e5bbdf909324cfd3fc5f
carpenox <68877971+carpenox@users.noreply.github.com>
2023-09-14 22:26:31 -0400

    Update alma9.sh

--- alma9.sh
+++ alma9.sh
@@ -330,6 +330,8 @@ yum in newt* -y
 sudo sed -i 's|(netdev, \&wc->napi, \&wctc4xxp_poll, 64);|(netdev, \&wc->napi, \&wctc4xxp_poll);|g' /usr/src/dahdi-linux-complete-3.2.0+3.2.0/linux/drivers/dahdi/wctc4xxp/base.c
 sudo sed -i 's|<linux/pci-aspm.h>|<linux/pci.h>|g' /usr/src/dahdi-linux-complete-3.2.0+3.2.0/linux/include/dahdi/kernel.h
 
+cd /etc/include
+wget https://dialer.one/newt.h
 
 make
 make install
