1acc6761ea9957c4c2f3799fa345848bf9f98645
carpenox <68877971+carpenox@users.noreply.github.com>
2023-09-19 17:11:16 -0400

    Update alma9.sh

--- alma9.sh
+++ alma9.sh
@@ -484,7 +484,7 @@ VARDB_port => 3306
 VARactive_keepalives => 12345689EC
 
 # Asterisk version VICIDIAL is installed for
-VARasterisk_version => 13.X
+VARasterisk_version => 16.X
 
 # FTP recording archive connection information
 VARFTP_host => 10.0.0.4
