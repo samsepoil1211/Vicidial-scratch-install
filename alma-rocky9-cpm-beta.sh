0f7b39506eb5025ce7cb271dc680bcef3cb1362a
carpenox <68877971+carpenox@users.noreply.github.com>
2023-10-25 00:01:32 -0400

    Update alma-rocky9-cpm-beta.sh

--- alma-rocky9-cpm-beta.sh
+++ alma-rocky9-cpm-beta.sh
@@ -430,6 +430,12 @@ perl install.pl --no-prompt
 #Install Crontab
 cat <<CRONTAB>> /root/crontab-file
 
+###certbot renew
+51 23 1 * * /usr/bin/systemctl stop firewalld
+52 23 1 * * /usr/sbin/certbot renew
+53 23 1 * * /usr/bin/systemctl start firewalld
+54 23 1 * * /usr/bin/systemctl restart httpd
+
 ### recording mixing/compressing/ftping scripts
 #0,3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57 * * * * /usr/share/astguiclient/AST_CRON_audio_1_move_mix.pl
 0,3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57 * * * * /usr/share/astguiclient/AST_CRON_audio_1_move_mix.pl --MIX
@@ -709,6 +715,13 @@ WELCOME
 
 chmod 777 /var/spool/asterisk/monitorDONE
 chkconfig asterisk off
+
+systemctl enable certbot-renew.timer
+systemctl start certbot-renew.timer
+cd /root/vicidial-install-scripts
+chmod +x vicidial-enable-webrtc.sh
+./vicidial-enable-webrtc.sh
+
 
 read -p 'Press Enter to Reboot: '
 
