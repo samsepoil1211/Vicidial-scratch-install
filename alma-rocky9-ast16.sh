84f9eb045f73aee0019a3db8dc4c8fc6916e5a88
carpenox <68877971+carpenox@users.noreply.github.com>
2023-11-18 11:38:04 -0500

    Update alma-rocky9-ast16.sh

--- alma-rocky9-ast16.sh
+++ alma-rocky9-ast16.sh
@@ -514,7 +514,12 @@ cat <<CRONTAB>> /root/crontab-file
 ### Dynportal
 @reboot /usr/bin/VB-firewall --whitelist=ViciWhite --dynamic --quiet
 * * * * * /usr/bin/VB-firewall --whitelist=ViciWhite --dynamic --quiet
-
+* * * * * /usr/local/bin/VB-firewall.pl --white --dynamic --quiet
+* * * * * sleep 10; /usr/local/bin/VB-firewall.pl --white --dynamic --quiet
+* * * * * sleep 20; /usr/local/bin/VB-firewall.pl --white --dynamic --quiet
+* * * * * sleep 30; /usr/local/bin/VB-firewall.pl --white --dynamic --quiet
+* * * * * sleep 40; /usr/local/bin/VB-firewall.pl --white --dynamic --quiet
+* * * * * sleep 50; /usr/local/bin/VB-firewall.pl --white --dynamic --quiet
 
 CRONTAB
 
