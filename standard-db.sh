6c2d7727e67fead113774e78f9984bb92f068c75
carpenox <68877971+carpenox@users.noreply.github.com>
2023-12-09 20:01:23 -0500

    Update standard-db.sh

--- standard-db.sh
+++ standard-db.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-wget https://cybur-dial.com/standard-db.zip
+wget https://cybur-dial.com/downloads/standard-db.zip
 unzip standard-db.zip
 mysql -u root asterisk < standard-db
 /usr/share/astguiclient/ADMIN_update_server_ip.pl --old-server_ip=10.10.10.15
