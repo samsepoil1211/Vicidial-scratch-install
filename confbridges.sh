84ab12e3ef950a9ef79bcab9fa11702ad2cde00f
carpenox <68877971+carpenox@users.noreply.github.com>
2023-12-15 10:46:13 -0500

    Update confbridges.sh

--- confbridges.sh
+++ confbridges.sh
@@ -1,3 +1,5 @@
+
+
 cd /usr/src/asterisk/asterisk-18.18.1/
 wget http://download.vicidial.com/asterisk-patches/Asterisk-18/amd_stats-18.patch
 wget http://download.vicidial.com/asterisk-patches/Asterisk-18/iax_peer_status-18.patch
@@ -17,3 +19,15 @@ patch < sip_peer_status-18.patch channels/chan_sip.c
 patch < timeout_reset_dial_app-18.patch apps/app_dial.c
 patch < timeout_reset_dial_core-18.patch main/dial.c
 
+tee -a /etc/php.ini <<EOF
+
+error_reporting  =  E_ALL & ~E_NOTICE
+memory_limit = 448M
+short_open_tag = On
+max_execution_time = 3330
+max_input_time = 3360
+post_max_size = 448M
+upload_max_filesize = 442M
+default_socket_timeout = 3360
+date.timezone = America/New_York
+EOF
