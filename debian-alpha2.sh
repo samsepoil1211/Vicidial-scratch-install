fd24f66aff5600fc04fed7616a960a462090a47a
carpenox <68877971+carpenox@users.noreply.github.com>
2024-03-10 16:01:42 -0400

    Update debian-alpha2.sh

--- debian-alpha2.sh
+++ debian-alpha2.sh
@@ -1,6 +1,214 @@
 #!/bin/sh
 
 echo "Vicidial installation Debian 12 with WebPhone(WebRTC/SIP.js)"
+apt install build-essential
+apt install flex git python3-pip yum
+##yum groupinstall "Development Tools" -y
+
+apt install -y apt-transport-https lsb-release ca-certificates wget
+wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
+sudo apt update
+apt install -y php7.4 
+apt install -y mariadb-server
+ 
+apt install libjansson* libedit* -y
+apt -y install dnf-plugins-core
+sudo apt install sqlite3 libsqlite3-dev -y
+apt-get install linux-headers-generic -y
+
+apt install -y php7.4 php7.4-opcache screen php7.4-mcrypt php-pear libmcrypt-dev mcrypt byobu screenie iselect db5.3-util libapache2-mod-svn subversion-tools subversion php7.4-cli php7.4-gd php7.4-curl php7.4-mysql php7.4-ldap php7.4-zip php7.4-common
+apt install -y wget unzip make patch subversion php7.4-mbstring apache2
+apt install -y php7.4-imap php7.4-ldap php7.4-mysqli php7.4-odbc php-pear php7.4-xml php7.4-xmlrpc curlk 
+##apt install -y newt-devel libxml2-devel kernel-devel sqlite-devel libuuid-devel sox sendmail lame-devel htop iftop perl-File-Which
+##apt install -y php-opcache libss7 mariadb-devel libss7* libopen* 
+##apt install -y sqlite-devel httpd mod_ssl nano chkconfig htop atop mytop iftop
+apt install -y uuid* libxml2*
+
+cpan> install Bundle::CPAN
+cpan> reload cpan
+cpan> install YAML
+cpan> install MD5
+cpan> install Digest::MD5
+cpan> install Digest::SHA1
+cpan> install readline
+cpan> reload cpan
+cpan> install DBI
+cpan> force install DBD::mysql
+cpan> install Net::Telnet
+cpan> install Time::HiRes
+cpan> install Net::Server
+cpan> install Switch
+cpan> install Mail::Sendmail
+cpan> install Unicode::Map
+cpan> install Jcode
+cpan> install Spreadsheet::WriteExcel
+cpan> install OLE::Storage_Lite
+cpan> install Proc::ProcessTable
+cpan> install IO::Scalar
+cpan> install Spreadsheet::ParseExcel
+cpan> install Curses
+cpan> install Getopt::Long
+cpan> install Net::Domain
+cpan> install Term::ReadKey
+cpan> install Term::ANSIColor
+cpan> install Spreadsheet::XLSX
+cpan> install Spreadsheet::Read
+cpan> install LWP::UserAgent
+cpan> install HTML::Entities
+cpan> install HTML::Strip
+cpan> install HTML::FormatText
+cpan> install HTML::TreeBuilder
+cpan> install Time::Local
+cpan> install MIME::Decoder
+cpan> install Mail::POP3Client
+cpan> install Mail::IMAPClient
+cpan> install Mail::Message
+cpan> install IO::Socket::SSL
+cpan> install MIME::Base64
+cpan> install MIME::QuotedPrint
+cpan> install Crypt::Eksblowfish::Bcrypt
+cpan> quit 
+
+
+apt install libsrtp* -y
+
+### up to this point
+
+tee -a /etc/apache2/apache2.conf <<EOF
+
+CustomLog /dev/null common
+
+Alias /RECORDINGS/MP3 "/var/spool/asterisk/monitorDONE/MP3/"
+
+<Directory "/var/spool/asterisk/monitorDONE/MP3/">
+    Options Indexes MultiViews
+    AllowOverride None
+    Require all granted
+</Directory>
+EOF
+
+sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+
+tee -a /etc/php/7.4/apache2/php.ini <<EOF
+
+error_reporting  =  E_ALL & ~E_NOTICE
+memory_limit = 448M
+short_open_tag = On
+max_execution_time = 3330
+max_input_time = 3360
+post_max_size = 448M
+upload_max_filesize = 442M
+default_socket_timeout = 3360
+date.timezone = America/New_York
+EOF
+
+
+systemctl restart apache2
+
+
+apt install -y mysql*
+
+apt-get install dnf-plugins-core -y
+#dnf config-manager --set-enabled powertools
+
+
+systemctl enable mysql
+
+cp /etc/my.cnf /etc/my.cnf.original
+echo "" > /etc/my.cnf
+
+
+cat <<MYSQLCONF>> /etc/my.cnf
+[mysql.server]
+user = mysql
+#basedir = /var/lib
+
+[client]
+port = 3306
+socket = /var/lib/mysql/mysql.sock
+
+[mysqld]
+datadir = /var/lib/mysql
+#tmpdir = /home/mysql_tmp
+socket = /var/lib/mysql/mysql.sock
+user = mysql
+old_passwords = 0
+ft_min_word_len = 3
+max_connections = 800
+max_allowed_packet = 32M
+skip-external-locking
+sql_mode="NO_ENGINE_SUBSTITUTION"
+
+log-error = /var/log/mysqld/mysqld.log
+
+query-cache-type = 1
+query-cache-size = 32M
+
+long_query_time = 1
+#slow_query_log = 1
+#slow_query_log_file = /var/log/mysqld/slow-queries.log
+
+tmp_table_size = 128M
+table_cache = 1024
+
+join_buffer_size = 1M
+key_buffer = 512M
+sort_buffer_size = 6M
+read_buffer_size = 4M
+read_rnd_buffer_size = 16M
+myisam_sort_buffer_size = 64M
+
+max_tmp_tables = 64
+
+thread_cache_size = 8
+thread_concurrency = 8
+
+# If using replication, uncomment log-bin below
+#log-bin = mysql-bin
+
+[mysqldump]
+quick
+max_allowed_packet = 16M
+
+[mysql]
+no-auto-rehash
+
+[isamchk]
+key_buffer = 256M
+sort_buffer_size = 256M
+read_buffer = 2M
+write_buffer = 2M
+
+[myisamchk]
+key_buffer = 256M
+sort_buffer_size = 256M
+read_buffer = 2M
+write_buffer = 2M
+
+[mysqlhotcopy]
+interactive-timeout
+
+[mysqld_safe]
+#log-error = /var/log/mysqld/mysqld.log
+#pid-file = /var/run/mysqld/mysqld.pid
+MYSQLCONF
+
+mkdir /var/log/mysqld
+touch /var/log/mysqld/slow-queries.log
+chown -R mysql:mysql /var/log/mysqld
+systemctl restart mariadb
+
+systemctl enable apache2.service
+systemctl enable mariadb.service
+systemctl restart apache2.service
+systemctl restart mariadb.service
+
+#Install Perl Modules
+
+echo "Install Perl"
+
+apt install -y perl-CPAN perl-YAML perl-CPAN-DistnameInfo perl-libwww-perl perl-DBI perl-DBD-MySQL perl-GD perl-Env perl-Term-ReadLine-Gnu perl-SelfLoader perl-open.noarch 
 
 sudo add-apt-repository ppa:ondrej/php  -y
 sudo apt-get update
