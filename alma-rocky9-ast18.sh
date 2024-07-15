688c650948a2696a63db30c85d138d1894087317
carpenox <68877971+carpenox@users.noreply.github.com>
2024-07-15 14:07:19 -0400

    Update alma-rocky9-ast18.sh

--- alma-rocky9-ast18.sh
+++ alma-rocky9-ast18.sh
@@ -218,7 +218,7 @@ wget https://cybur-dial.com/dahdi-9.4-fix.zip
 unzip dahdi-9.4-fix.zip
 yum in newt* -y
 
-## sudo sed -i 's|(netdev, \&wc->napi, \&wctc4xxp_poll, 64);|(netdev, \&wc->napi, \&wctc4xxp_poll);|g' /usr/src/dahdi-linux-complete-3.2.0+3.2.0/linux/drivers/dahdi/wctc4xxp/base.c
+## sudo sed -i 's|(netdev, \&wc->napi, \&wctc4xxp_poll, 64);|(netdev, \&wc->napi, \&wctc4xxp_poll);|g' /usr/src/dahdi-linux-complete-3.4.0-rc1+3.4.0-rc1/linux/drivers/dahdi/wctc4xxp/base.c
 ## sudo sed -i 's|<linux/pci-aspm.h>|<linux/pci.h>|g' /usr/src/dahdi-linux-complete-3.2.0+3.2.0/linux/include/dahdi/kernel.h
 
 make clean
