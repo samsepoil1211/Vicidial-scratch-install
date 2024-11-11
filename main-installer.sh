a58a32702cc5bd214fc2e667321c3d8d71e40850
carpenox <68877971+carpenox@users.noreply.github.com>
2024-11-11 07:32:50 -0500

    Update main-installer.sh

--- main-installer.sh
+++ main-installer.sh
@@ -14,8 +14,8 @@ prompt() {
 echo "Getting Machine info"
 echo "**************************************************************************"
 prompt hostname "Enter the hostname:" "$hostname"
-read
 echo "Press Enter to continue"
+read
 hostnamectl set-hostname $hostname
 # Retrieve the Hostname
 hostname=$(hostname | awk '{print $1}')
