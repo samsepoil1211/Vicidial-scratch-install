cf3af6d046d211536ea8319a4b49c0e9f9e3d6aa
carpenox <68877971+carpenox@users.noreply.github.com>
2024-11-24 19:27:33 -0500

    Create run-on-dialer-servers-cluster.sh

---
+++ run-on-dialer-servers-cluster.sh
@@ -0,0 +1,7 @@
+echo "Enter the database IP:"
+read DB
+
+perl /usr/src/astguiclient/trunk/install.pl --DB_server=$DB --no-prompt
+asterisk -rx 'core restart now'
+sleep 2
+/usr/share/astguiclient/ADMIN_keepalive_ALL.pl
