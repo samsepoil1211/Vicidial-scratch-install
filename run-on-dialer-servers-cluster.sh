be829b6b5216148c58a6eafa6df79883df081dbc
carpenox <68877971+carpenox@users.noreply.github.com>
2025-02-23 17:51:15 -0500

    Update run-on-dialer-servers-cluster.sh

--- run-on-dialer-servers-cluster.sh
+++ run-on-dialer-servers-cluster.sh
@@ -5,3 +5,5 @@ perl /usr/src/astguiclient/trunk/install.pl --DB_server=$DB --no-prompt
 asterisk -rx 'core restart now'
 sleep 2
 /usr/share/astguiclient/ADMIN_keepalive_ALL.pl
+sleep 5
+perl /usr/src/astguiclient/trunk/install.pl --no-prompt
