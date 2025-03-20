7749587d04ec8b28a380407cc8c87e75216f2324
carpenox <68877971+carpenox@users.noreply.github.com>
2025-03-19 21:55:36 -0400

    Update standard-db.sh

--- standard-db.sh
+++ standard-db.sh
@@ -8,6 +8,7 @@ svn up
 perl install.pl --no-prompt
 mysql -p -f --database=asterisk < /usr/src/astguiclient/trunk/extras/upgrade_2.14.sql
 echo "Just press enter"
+mysql asterisk -u cron -p1234 -e "update vicidial_users set phone_login=concat(user,'a');"
 echo "The default password for the 6666 admin user is CyburDial2024"
 echo "The default password for the 6666 admin user is CyburDial2024"
 echo "The default password for the 6666 admin user is CyburDial2024"
